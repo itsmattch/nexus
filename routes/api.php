e5df0096293bb10ab0751d6aee2b1813b83685b6
itsmattch <hello@itsmattch.com>
2023-08-14 15:55:40 +0200

    Reject api endpoints falling back to web

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use Itsmattch\NexusHeadless\Facades\Nexus;
 
 /*
@@ -14,3 +15,14 @@ use Itsmattch\NexusHeadless\Facades\Nexus;
 */
 
 Nexus::routes();
+
+Route::any('{any}', function () {
+    return response()->json([
+        'message' => 'The requested resource or HTTP method is not supported.',
+        'errors' => [
+            'general' => [
+                'The requested resource or HTTP method is not supported.'
+            ]
+        ]
+    ], 404);
+})->where('any', '.*');
