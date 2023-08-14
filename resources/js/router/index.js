a68d0e66c6f156c8d689b97306d0d3788c9e1432
itsmattch <hello@itsmattch.com>
2023-08-14 08:35:43 +0200

    Remove unnecessary routes

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,19 +1,13 @@
 import {createRouter, createWebHistory} from 'vue-router';
 
-import EntitiesIndex from "../sfc/pages/Entities/Index.vue";
-import EntitiesShow from "../sfc/pages/Entities/Show.vue";
-import Processes from "../sfc/pages/Processes.vue";
-import Resources from "../sfc/pages/Resources.vue";
+import EntitiesIndex from "../sfc/pages/entities/Index.vue";
+import EntitiesShow from "../sfc/pages/entities/Show.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {name: 'entities.index', path: '/entities', component: EntitiesIndex},
         {name: 'entities.show', path: '/entities/:id', component: EntitiesShow},
-
-        {name: 'processes.index', path: '/processes', component: Processes},
-
-        {name: 'resources.index', path: '/resources', component: Resources}
     ]
 })
 
